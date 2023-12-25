ec8c037166ee7e2076853972193d508b79169d95
warot020210 <65160235@go.buu.ac.th>
2023-12-26 00:48:57 +0700

    new

--- PHP/index.php
+++ PHP/index.php
@@ -4,18 +4,33 @@
         <meta chatset="utf-8"/>
         <link rel ="stylesheet" href="stylephp.css">
     </head>
+    
     <body>
-        
+        <center>
+            <div class = head1>
+                <h1 >multiplication table</h1>
+            </div>
+            
         <?php $multi_x = 2; $lenght_number =24; ?>
-        <table>
+        <div class =multitable>
+            <table>
                 <?php
                 for ($j = 1; $j <= $lenght_number; $j++) {
                     echo "<tr><td>" . $multi_x . ' x ' . $j . ' = ' . ($multi_x * $j) . "<br><br>\n";
                 }
                 ?>
         </table>
+        
+        </div>
+        
+        </center>
+        
+        
+        
        
     </body>
+   
+   
 </html>
 
 
